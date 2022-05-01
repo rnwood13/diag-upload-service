14b67b5e6f3e89c311ea43d2b27b0fa58a005633
Robert Wood <rnwood@outlook.com>
2022-05-01 17:43:42 -0500

    Add variables to control ECS and ELB deployment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.diag_upload_service.repository_url
 }
 
-output "alb_url" {
-  value = "http://${module.alb.lb_dns_name}"
-}
+# output "alb_url" {
+#   value = "http://${module.alb.lb_dns_name}"
+# }
