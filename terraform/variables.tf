afd79af658ed78c8862da282797362392ce90f7a
Robert Wood <rnwood@outlook.com>
2022-05-02 14:54:50 -0500

    Update ECR image tag

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,54 +1,71 @@
 variable "project_name" {
-  type    = string
-  default = "MyProject"
+  description = "Name of the project"
+  type        = string
+  default     = "MyProject"
 }
 
 variable "vpc_region" {
-  type    = string
-  default = "us-east-2"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
 }
 
 variable "vpc_name" {
-  type    = string
-  default = "MyVPC"
+  description = "Name of the custom VPC for this project"
+  type        = string
+  default     = "MyVPC"
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  description = "CIDR range for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "vpc_azs" {
-  type    = list(string)
-  default = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  description = "Availability zones to use"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
 }
 
 variable "vpc_private_subnets" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "CIDR networks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "vpc_public_subnets" {
-  type    = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  description = "CIDR networks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "vpc_enable_nat_gateway" {
-  type    = bool
-  default = false
+  description = "Whether to enable the NAT Gateway"
+  type        = bool
+  default     = false
 }
 
 variable "vpc_enable_vpn_gateway" {
-  type    = bool
-  default = false
+  description = "Whether to enable VPN Gateway"
+  type        = bool
+  default     = false
 }
 
 variable "ecs_enable" {
-  type    = bool
-  default = false
+  description = "Whether to enable and deploy ECS"
+  type        = bool
+  default     = false
 }
 
 variable "elb_enable" {
-  type    = bool
-  default = false
+  description = "Whether to enable Elastic Load Balancer"
+  type        = bool
+  default     = false
+}
+
+variable "ecr_image_tag" {
+  description = "Default image tag - will be overwritten in CI"
+  type        = string
+  default     = "latest"
 }
