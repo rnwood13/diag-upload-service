4a4d9119d40150929239313d14a3369a76dad874
Robert Wood <rnwood@outlook.com>
2022-05-03 13:12:02 -0500

    Add ability to list uploaded files

--- app/index.js
+++ app/index.js
@@ -1,17 +1,20 @@
 const fileUpload = require('express-fileupload');
-// const basicAuth = require('express-basic-auth')
-const express = require('express')
+// const basicAuth = require('express-basic-auth');
+const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const port = 8000;
 
-const diagDir = 'diags'
+const diagDir = path.join(__dirname, 'diags');
+// const directoryPath = path.join(__dirname, diagDir);
 app.use(fileUpload());
-app.use(express.static(diagDir))
+app.use(express.static(diagDir));
+
 // app.use(basicAuth({
 //     users: { 'admin': 'admin' },
 //     challenge: true
-// }))
+// }));
 
 app.post('/upload', (req, res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
@@ -23,20 +26,32 @@ app.post('/upload', (req, res) => {
             console.error(err);
             return res.status(500).send(err);
         } else {
-            console.log(`File ${diag.name} uploaded`)
+            console.log(`File ${diag.name} uploaded`);
             res.send(`File ${diag.name} uploaded`);
         }
     });
-})
+});
 
 app.get('/download/:id', (req, res) => {
     res.sendFile(req.params.id, {root: diagDir})
-})
+});
 
 app.get('/', (req, res) => {
+    fs.readdir(diagDir, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files in EFS
+        files.forEach(function (file) {
+            // Display list of files to console
+            console.log(file); 
+        });
+    });
+
+    // Present HTML page
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}!`)
+    console.log(`App is listening on port ${port}!`);
 });
